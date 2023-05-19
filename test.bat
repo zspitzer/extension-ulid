cce7d8d35483ddedc627b20c1d100de1c4c51db3
Zac Spitzer <zac.spitzer@gmail.com>
2023-05-19 12:52:07 +0200

    cleanup

--- test.bat
+++ test.bat
@@ -5,4 +5,4 @@ set testFilter=
 set testAdditional=C:\work\lucee-extensions\extension-ulid\tests
 set testServices=mysql
 
-ant -buildfile="C:\work\script-runner" -DluceeVersion="6.0.0.391-SNAPSHOT" -Dwebroot="C:\work\lucee6\test" -Dexecute="/bootstrap-tests.cfm" -DextensionDir="C:\work\lucee-extensions\extension-ulid\dist" -DextensionDir="C:\work\lucee-extensions\extension-ulid\dist"
+ant -buildfile="C:\work\script-runner" -DluceeVersion="light-6.0.0.391-SNAPSHOT" -Dwebroot="C:\work\lucee6\test" -Dexecute="/bootstrap-tests.cfm" -DextensionDir="C:\work\lucee-extensions\extension-ulid\dist" -DextensionDir="C:\work\lucee-extensions\extension-ulid\dist"
--- tests/installed.cfc
+++ tests/installed.cfc
@@ -2,8 +2,8 @@ component extends="org.lucee.cfml.test.LuceeTestCase" labels="ulid" {
     function testInstalled (){
         var fl = getFunctionList();
         expect( fl ) .toHaveKey( "createULID" );
-        var fd= getFunctionData("createULID");
-        systemOutput(fd, true);
+        var fd = getFunctionData( "createULID" );
+        expect( arrayLen( fd.arguments ) ).toBe( 3 );
         createULID();
     }
 }
